1a74a8d5cfd269e460e8345115f33296f34037dd
Adrian Borucki <gentoolx@gmail.com>
2011-09-03 01:25:22 +0200

    Additional commit because the last one was uncomplete.

--- usercreate.sh
+++ usercreate.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 # This file creates user and group of name 'wintermute' to allow Wintermute access its internals
 # located under directories not belonging to the user running.
 
+echo "*** Creating user and group of name 'wintermute'... ***"
 if [ -e /usr/bin/sudo ]; then
   sudo useradd -r -d /var/lib/wintermute -s /bin/false wintermute
 else
