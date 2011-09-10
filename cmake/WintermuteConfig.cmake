1c5523c153efc9d66565d4085d63eb4d2e20a68b
Jacky Alcine <jacky.alcine@thesii.org>
2011-09-10 00:43:20 -0400

    More work to have previous merge working.

---
+++ cmake/WintermuteConfig.cmake
@@ -0,0 +1,8 @@
+# - Config file for the WINTERMUTE package
+# It defines the following variables
+#  WINTERMUTE_INCLUDE_DIRS - include directories for WINTERMUTE
+#  WINTERMUTE_LIBRARY_DIRS - library directories for WINTERMUTE (normally not used!)
+
+# Tell the user project where to find our headers and libraries
+set(WINTERMUTE_INCLUDE_DIRS "/usr/include/wntr/ling;/usr/include/wntr/data;/usr/include/python2.7;/usr/include/qt4;/usr/include;/usr/include/python2.7;/usr/include;/usr/include/qt4;/usr/include/wntr/data;/usr/include/python2.7;/usr/include/qt4;/usr/include;/usr/include/wntr/ntwk;/usr/include;/usr/include/qt4;/usr/include/python2.7;/usr/include;/usr/include;/usr/include/qt4/QtGui")
+set(WINTERMUTE_LIBRARY_DIRS "")
