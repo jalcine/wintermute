3b755df7e35381704ce53390c2b3e4e50411e851
Adrian Borucki <gentoolx@gmail.com>
2012-01-18 22:46:25 +0100

    Header now installed correctly + few cmake scripts corrections.

--- cmake/Modules/WintermuteMacros.cmake
+++ cmake/Modules/WintermuteMacros.cmake
@@ -35,22 +35,22 @@ MACRO(PKGCONFIG_GETVAR _package _var _output_variable)
   SET(${_output_variable})
 
   # if pkg-config has been found
-  IF(PKGCONFIG_EXECUTABLE)
+  IF(PKG_CONFIG_FOUND)
 
-    EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --exists RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
+    EXEC_PROGRAM(${PKG_CONFIG_EXECUTABLE} ARGS ${_package} --exists RETURN_VALUE _return_VALUE OUTPUT_VARIABLE _pkgconfigDevNull )
 
     # and if the package of interest also exists for pkg-config, then get the information
     IF(NOT _return_VALUE)
 
-      EXEC_PROGRAM(${PKGCONFIG_EXECUTABLE} ARGS ${_package} --variable ${_var} OUTPUT_VARIABLE ${_output_variable} )
+      EXEC_PROGRAM(${PKG_CONFIG_EXECUTABLE} ARGS ${_package} --variable ${_var} OUTPUT_VARIABLE ${_output_variable} )
 
     ELSE(NOT _return_VALUE)
       MESSAGE(WARNING "${_package} not found.")
     ENDIF(NOT _return_VALUE)
 
-  ELSE(PKGCONFIG_EXECUTABLE)
+  ELSE(PKG_CONFIG_FOUND)
     MESSAGE(ERROR "PkgConfig not found.")
-  ENDIF(PKGCONFIG_EXECUTABLE)
+  ENDIF(PKG_CONFIG_FOUND)
 
 ENDMACRO(PKGCONFIG_GETVAR _package _var _output_variable)
 
