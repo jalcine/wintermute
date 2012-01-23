2a9015b9768999f58b480e538e0d0c4ea09cd59d
Jacky Alcine <jackyalcine@gmail.com>
2012-01-23 11:10:00 -0500

    Adding diagonstic system.

---
+++ src/diagnoser.hpp
@@ -0,0 +1,73 @@
+#ifndef _DIAGNOSER_HPP_
+#define _DIAGNOSER_HPP_
+
+// Qt
+#include <QObject>
+#include <QString>
+#include <QMap>
+
+namespace Wintermute {
+namespace Diagnostics {
+
+/**
+ * @brief Base diagnoser class
+ * This class represents one of major WCore's components: diagnostics performer
+ * & reporter. Its role is to provide statistics about system's vitality and
+ * its used for reporting and self-assesment.
+ */
+class Diagnoser : public QObject
+{
+    Q_OBJECT
+
+private:
+    static Diagnoser* coreDiagnoser; /// Diagnoser responsible for Core monitoring.
+    static QMap<QString, Diagnoser*> diagnosers; /// Other diagnosers, responsible mainly for plug-ins.
+
+protected:
+    QString label;
+
+public:
+    Diagnoser(QObject* = 0);
+
+    /**
+     * @brief Construct a diagnoser using label
+     * @param newLabel Diagnoser's label.
+     * @param autoRegister If true, automatically registers itself.
+     * @param parent QObject parent.
+     */
+    Diagnoser(QString&, bool = true, QObject* = 0);
+
+    /**
+     * @brief Get diagnoser's label
+     * @returns Label of a diagnoser.
+     */
+    QString getLabel();
+
+    /**
+     * @brief Register new diagnoser
+     * @param pDiag Pointer to the diagnoser.
+     */
+    static void registerDiagnoser(Diagnoser*);
+
+    /**
+     * @brief Remove diagnoser
+     * @param label Label of a diagnoser you want to remove.
+     */
+    static void removeDiagnoser(QString&);
+
+    /**
+     * @brief Get pointer to the Core Diagnoser
+     * @returns Pointer to the Core Diagnoser.
+     */
+    static Diagnoser* getCoreDiagnoser();
+
+    /**
+     * @brief Get pointer to a diagnoser with given label
+     * @param label Label of the wanted diagnoser.
+     * @returns Pointer to the diagnoser with the given label.
+     */
+    static Diagnoser* getDiagnoser(QString&);
+};
+} // namespaces
+}
+#endif // _DIAGNOSER_HPP_
