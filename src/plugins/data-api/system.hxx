f87cc66e7b3908d1ee9754d9172d1c34c1570dfd
Jacky Alcine <jackyalcine@gmail.com>
2012-04-09 08:10:00 -0400

    Added private header.

---
+++ src/plugins/data-api/system.hxx
@@ -0,0 +1,27 @@
+/***
+ *  This file is part of Wintermute.
+ *
+ *  Copyright (C) 2012 Jacky Alciné <jackyalcine@gmail.com>
+ *
+ *  Wintermute is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  Wintermute is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public License
+ *  along with Wintermute .  If not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+
+namespace Wintermute { namespace Data {
+    struct SystemPrivate {
+QString m_dir;
+    };
+}
+}
