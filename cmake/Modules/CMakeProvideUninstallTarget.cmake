de581b0c254f0f844b2550364bb924664d17bd8d
Jacky Alciné <jacky.alcine@thesii.org>
2013-03-04 09:21:24 -0500

    Add modules used by Wintermute.

---
+++ cmake/Modules/CMakeProvideUninstallTarget.cmake
@@ -0,0 +1,40 @@
+# TODO: Add default CMake banner here.
+###############################################################################
+### Copyright (C) 2013 Jacky Alcine <jacky.alcine@thesii.org>
+###
+### This file is part of Wintermute, the extensible AI platform.
+###
+### Wintermute is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation; either version 3 of the License, or
+### (at your option) any later version.
+###
+### Wintermute is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with Wintermute.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+# Only execute the following block if the 'uninstall' target doesn't exist.
+if (NOT TARGET uninstall)
+
+  # Check if the template exists.
+  find_file(CMAKE_UNINSTALL_TEMPLATE_FILE cmake_uninstall.cmake.in
+            PATHS ${CMAKE_MODULE_PATH} "${CMAKE_CURRENT_SOURCE_DIR}"
+            DOC "Path to the CMake uninstallation template.")
+
+  # Proceed only if the file exists.         
+  if (EXISTS CMAKE_UNINSTALL_TEMPLATE_FILE)
+    # Configure the file accordingly.
+    configure_file(${CMAKE_UNINSTALL_TEMPLATE_FILE} 
+                  "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake" @ONLY)
+
+    # Define the 'uninstall' target.
+    add_custom_target(uninstall
+      "${CMAKE_COMMAND}" -P "${CMAKE_BINARY_DIR}/cmake_uninstall.cmake")
+  endif(EXISTS CMAKE_UNINSTALL_TEMPLATE_FILE)
+
+endif(NOT TARGET uninstall)
---
+++ cmake/Modules/WintermuteDependencies.cmake
@@ -0,0 +1,49 @@
+## TODO: Add proper CMake module definition here.
+###############################################################################
+### Copyright (C) 2013 Jacky Alcine <jacky.alcine@thesii.org>
+###
+### This file is part of Wintermute, the extensible AI platform.
+###
+### Wintermute is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation; either version 3 of the License, or
+### (at your option) any later version.
+###
+### Wintermute is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with Wintermute.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+include(FeatureSummary)
+
+## Pass options to manipulate Wintermute's dependency.
+option(PROVIDE_GUI_SUPPORT      "Compile Wintermute with QtGui libraries linked and used as run-time." ON)
+
+## Define variables necessary for Qt discovery.
+set(WINTERMUTE_QT_VERSION "4.8.4")
+
+## {{{ Look for Qt, if not found already.
+if (NOT DEFINED QT_FOUND OR NOT "${WINTERMUTE_QT_VERSION}" EQUAL 
+    "${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH}")
+  find_package(Qt4 ${WINTERMUTE_QT_VERSION} COMPONENTS
+               QtCore
+  REQUIRED)
+endif()
+## }}}
+
+find_package(QCommandLine REQUIRED)
+
+## {{{ Finish up dependency inclusion.
+include("${QT_USE_FILE}")
+## }}}
+
+## {{{ Determine which features and dependencies are flipped on/off.
+add_feature_info("Gui Support" QT_QTGUI_FOUND "Allows for the rendering of graphical elements.")
+## }}}
+
+## Provide feature report.
+feature_summary(WHAT ALL)
