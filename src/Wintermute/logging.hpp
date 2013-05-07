3a2b78f05de008a7f9bab7e822f084c7d5d109f9
Jacky Alciné <jacky.alcine@thesii.org>
2013-05-07 15:28:21 -0400

    Added shortcuts for logging to console.
      * Added wlog(qobj) to faciliate the obtaining of a logging object.
      * Added wdebug(qobj,text) to make debugging easier.
      * Added wwarn(qobj,text) to make warning messages easier.

--- src/Wintermute/logging.hpp
+++ src/Wintermute/logging.hpp
@@ -51,4 +51,36 @@ namespace Wintermute {
   };
 }
 
+// Shortcuts for quick logging support.
+
+/**
+ * @fn wlog
+ * @param qobj An object based on QObject.
+ *
+ * Obtains a logger for the specific QObject.
+ */
+#define wlog(qobj) Wintermute::Logging::obtainLogger(qobj)
+
+/**
+ * @fn wdebug
+ * @param qobj An object based on QObject.
+ * @param text The text to be displayed for debugging.
+ *
+ * Obtains a logger for the specific QObject and then prints out a 
+ * line of text for debubgging view.
+ *
+ * TODO: Make this conditionally visible if WINTERMUTE_DEBUG isn't set.
+ */
+#define wdebug(qobj, text) wlog(qobj)->debug(text)
+
+/**
+ * @fn wwarn
+ * @param qobj An object based on QObject
+ * @param text The text to be displayed as a warning.
+ *
+ * TODO: Make this conditionally visibile if WINTERMUTE_WARNINGS isn't set.
+ */
+#define wwarn(qobj, text) wlog(qobj)->warn(text)
+
+
 #endif /* WINTERMUTE_LOGGING_HPP */
