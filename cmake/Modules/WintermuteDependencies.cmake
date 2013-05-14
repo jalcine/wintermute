ceaea41b63236f08b17f822b7b915648e0b6c838
Jacky Alciné <jacky.alcine@thesii.org>
2013-05-14 02:37:44 -0400

    Dropped Git dependency for Wintermute.

--- cmake/Modules/WintermuteDependencies.cmake
+++ cmake/Modules/WintermuteDependencies.cmake
@@ -36,13 +36,16 @@ endif()
 
 ## }}}
 
+## {{{ Packages
+
 find_package(QCommandLine 0.4.0 MODULE REQUIRED)
 find_package(log4qt REQUIRED)
-find_package(Git REQUIRED)
+
+## }}}
 
 ## {{{ Determine which features and dependencies are flipped on/off.
 
-add_feature_info("Gui Support" QT_QTGUI_FOUND "Allows for the rendering of graphical elements.")
+add_feature_info("GUI" QT_QTGUI_FOUND "Allows for the rendering of graphical elements.")
 add_feature_info("Command Line" QCOMMANDLINE_FOUND
   "Allows Wintermute to parse the command line.")
 add_feature_info("Logging" log4qt_FOUND "Incorporates logging support.")
