57ffb2095318243ff0108f022993432faaa77782
Jacky Alciné <me@jalcine.me>
2013-06-08 01:55:35 -0400

    Improved plug-in library discovery.
      * Fixed UUID conversion to sane ID string.
      * Ensured full Linux path discovery for libraries.
      * Introduced more documentation.

--- src/Wintermute/factory.cpp
+++ src/Wintermute/factory.cpp
@@ -26,6 +26,7 @@
 #include "temporaryplugin.hpp"
 #include <QDir>
 #include <QFile>
+#include <QCoreApplication>
 
 using namespace Wintermute;
 using Wintermute::Factory;
@@ -54,6 +55,9 @@ namespace Wintermute {
         QStringList files = pluginDefDir.entryList(QStringList() << "*.spec", QDir::Files);
         PluginList plugins;
 
+        wdebug(Factory::instance(), files.join(","));
+        wdebug(Factory::instance(), WINTERMUTE_PLUGIN_DEFINITION_DIR);
+
         Q_FOREACH(QString pluginFile, files){
           QString uuid = pluginFile.remove(".spec");
           wdebug(Wintermute::Application::instance(), QString("Found plugin file '%1'.").arg(uuid));
@@ -75,26 +79,36 @@ namespace Wintermute {
       }
 
       QSettings* obtainConfiguration(const QUuid& id) const {
-        const QString settingsPath = QString(WINTERMUTE_PLUGIN_DEFINITION_DIR "/" + id.toString() + ".spec");
+        const QString settingsPath = QString(WINTERMUTE_PLUGIN_DEFINITION_DIR "/" + id.toString() + ".spec").replace("}","").replace("{","");
+        
         if (QFile::exists(settingsPath)){
-          QSettings* settings = new QSettings(settingsPath);
+          QSettings* settings = new QSettings(settingsPath, QSettings::IniFormat, Factory::instance());
+          winfo(Factory::instance(), QString("Found configuraton for plug-in '%1' at '%2'.").arg(id.toString()).arg(settingsPath));
           return settings;
         }
 
+        wdebug(Factory::instance(), QString("Can't find configuration data for %1 at %2.").arg(id.toString()).arg(settingsPath));
         return 0;
       }
       
       QPluginLoader* obtainBinary(const QUuid& id) const {
         const QSettings* config = this->obtainConfiguration(id);
 
-        if (!config)
+        if (!config){
+          wdebug(Factory::instance(), QString("Can't find meta-configuration for plugin %1.").arg(id.toString()));
           return 0;
+        }
 
-        const QString libraryName = config->value("Plugin/LibraryName").toString();
-        QPluginLoader* loader = new QPluginLoader(libraryName);
+        const QString libraryName = config->value("Plugin/Library").toString();
+        QPluginLoader* loader = new QPluginLoader(QString(WINTERMUTE_PLUGIN_LIBRARY_DIR "/lib%1.so").arg(libraryName));
 
-        if (loader->fileName().isEmpty())
+        if (loader->fileName().isEmpty()){
+          wdebug(Factory::instance(), QString("Library not found on operating system: '%1'").arg(libraryName));
+          Q_FOREACH(QString libraryPath, QCoreApplication::libraryPaths()){
+            wdebug(Factory::instance(), QString("Available library path: %1").arg(libraryPath));
+          }
           return 0;
+        }
 
         return loader;
       }
