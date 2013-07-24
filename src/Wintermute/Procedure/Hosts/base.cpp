cc492bdd40fabf9ffe48b84d67e662101cc928d3
Jacky Alciné <me@jalcine.me>
2013-07-23 23:34:43 -0400

    Updated Hosts w/ source files.

--- src/Wintermute/Procedure/Hosts/base.cpp
+++ src/Wintermute/Procedure/Hosts/base.cpp
@@ -3,26 +3,26 @@
  *
  */
 #include "Wintermute/Procedure/Hosts/base.hpp"
-#include "private/Procedure/Hosts/base.hpp"
-#include "private/vendor/zeromq.hpp"
+#include "Wintermute/Procedure/Agents/base.hpp"
+#include "Wintermute/private/Procedure/Hosts/base.hpp"
+#include "Wintermute/logging.hpp"
 
-using Wintermute::Procedure::Hosts::Base;
+using namespace Wintermute::Procedure;
 
-Base::Base() { }
+Hosts::Base::Base() { }
 
-Base* Base::discoverHost( QString& const qualifiedName ) {
-  // TODO: Query network for existence of said host.
-  // TODO: Invoke the `selfResolve()` method on host and call data.
-  // TODO: Use returned data to form a Hosts::Base* object.
-  // TODO: Return if valid.
-  return 0;
+// NOTE: Very functional!
+Hosts::Base* Hosts::Base::discoverHost( QString& qualifiedName ) {
+  return Agents::Base::pingHost(qualifiedName, Agents::Base::PingQuickestHost).first();
 }
 
-bool Base::dispatchCall( Call* const call ) {
+bool Hosts::Base::dispatchCall( Call* const call ) {
+  // TODO: Inform network of impending call.
   // TODO: Obtain the host to be called upon.
   // TODO: Serialize the arguments of the call.
-  //
+  // TODO: Send call to all agents.
   return false;
 }
 
-Base::~Base() { }
+Hosts::Base::~Base() { 
+}
--- src/Wintermute/Procedure/Hosts/base.hpp
+++ src/Wintermute/Procedure/Hosts/base.hpp
@@ -11,49 +11,72 @@
 namespace Wintermute {
   namespace Procedure {
     namespace Hosts {
+      class BasePrivate;
       class Base {
+        Q_OBJECT;
+        Q_DISABLE_COPY(Base);
+        Q_DECLARE_PRIVATE(Base);
+        QSharedPointer<BasePrivate> d_ptr;
+
+
         public:
-          /**
-           * @ctor
-           * Handles the construction of this host.
-           */
-          explicit Base();
+        /**
+         * @ctor
+         * Handles the construction of this host.
+         */
+        explicit Base();
 
-          /**
-           * @dtor
-           * Handles the destruction of this host.
-           */
-          virtual ~Base();
+        /**
+         * @dtor
+         * Handles the destruction of this host.
+         */
+        virtual ~Base();
 
-          /**
-           * @fn fullName
-           * @brief Obtains name of host.
-           */
-          virtual QString fullName() const = 0;
+        /**
+         * @fn fullName
+         * @brief Obtains name of host.
+         *
+         * When discovering or querying for hosts, this method intends to
+         * return the fully qualified name.
+         */
+        virtual QString fullName() const = 0;
 
-          /**
-           * @fn discoverHost
-           *
-           * Searches for a host with the specified name.
-           */
-          static Base* discoverHost(QString& qualifiedName);
+        /**
+         * @fn discoverHost
+         *
+         * Searches for a host with the specified name and returns the
+         * host's information for invocation.
+         */
+        static Base* discoverHost(QString& qualifiedName);
+
+        /**
+         * @fn invokeCall
+         */
+        bool invokeCall(const QString& callMethod, const QVariantList& arguments);
 
         protected:
-          /**
-           * @slot callRecieved
-           *
-           * Handles the act of manipulating incoming calls.
-           */
-          Q_SLOT void callRecieved(Call* const call) = 0;
+        /**
+         * @slot callRecieved
+         *
+         * Handles the act of manipulating incoming calls.
+         */
+        Q_SLOT virtual void callRecieved(Call* const call) = 0;
 
-          /**
-           * @fn dispatchCall
-           * Handles the work of sending a call.
-           */
-          bool dispatchCall(Call* const call);
+        /**
+         * @fn dispatchCall
+         * Handles the work of sending a call.
+         */
+        bool dispatchCall(Call* const call);
+
+        /**
+         * @fn registerCall
+         * Adds a call as a valid call for this object.
+         */
+        bool registerCall(Call* const call);
       };
     }
   }
 }
+
 
 #endif /* end  */
