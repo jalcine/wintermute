8f7111855f2aced506b7ad0d186e05d6095e18ac
Jacky Alciné <me@jalcine.me>
2013-08-09 00:06:11 -0400

    Added start of privae

---
+++ src/Wintermute/private/Procedure/module.hpp
@@ -0,0 +1,16 @@
+namespace Wintermute {
+  namespace Procedure {
+    class Module;
+    class ModulePrivate
+    {
+    public:
+      ModulePrivate (Module* q_ptr);
+      virtual ~ModulePrivate ();
+
+    
+    private:
+      /* data */
+    };
+  } /* Procedure */ 
+  
+} /* Wintermute  */ 
