ec1c0b1d558462d848ea00147812126e12f8110c
Jacky Alciné <me@jalcine.me>
2013-08-09 21:33:51 -0400

    Added more logic for procedure calling.

---
+++ src/Wintermute/Procedure/call.hpp
@@ -0,0 +1,23 @@
+/**
+ * vim: ft=cpp
+ */
+
+#ifndef WINTERMUTE_CORE_PROCEDURE_CALL_HPP
+#define WINTERMUTE_CORE_PROCEDURE_CALL_HPP
+
+#include <QtCore/QObject>
+
+namespace Wintermute {
+  namespace Procedure {
+    class Call : public QObject {
+      Q_OBJECT;
+      Q_DISABLE_COPY(Call);
+
+      public:
+        explicit Call (QObject* parent);
+        virtual ~Call ();
+    };
+  } /* Procedure */
+} /* Wintermute */
+
+#endif /* WINTERMUTE_CORE_PROCEDURE_CALL_HPP */
