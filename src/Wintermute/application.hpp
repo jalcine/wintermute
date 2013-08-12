c578db34996b360afe4170a47409c7b05b549efa
Jacky Alciné <me@jalcine.me>
2013-08-12 02:10:02 -0400

    Added three new methods.
      Added two modules for obtaining and setting the default module of a
      Wintermute process and one for obtaining the call name of a Wintermute
      process.

--- src/Wintermute/application.hpp
+++ src/Wintermute/application.hpp
@@ -17,13 +17,14 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Wintermute.  If not, see <http://www.gnu.org/licenses/>.
-**/
+ **/
 
 #ifndef WINTERMUTE_CORE_APPLICATION_HPP
 #define WINTERMUTE_CORE_APPLICATION_HPP
 
 #include <QtCore/QVariant>
 #include <Wintermute/Globals>
+#include <Wintermute/Procedure/Module>
 
 namespace Wintermute {
   class Version;
@@ -38,7 +39,8 @@ namespace Wintermute {
    */
   class Application : public QObject {
     Q_OBJECT;
-    Q_DECLARE_PRIVATE(Application);  
+    Q_DECLARE_PRIVATE(Application);
+    Q_DISABLE_COPY(Application);
 
     QScopedPointer<ApplicationPrivate> d_ptr;
     static Application* self;
@@ -54,68 +56,99 @@ namespace Wintermute {
     Application(int &argc, char **argv);
 
     public:
-      virtual ~Application();
-      /**
-       * @fn instance
-       * @brief Provides a pointer to an instance of Wintermute::Application.
-       *
-       * In order to keep Wintermute's instance running smoothly, 
-       * a shared pointer to the instance of the application that's running 
-       * and handling Wintermute's lower-level work. It's strongly recommended 
-       * that you use this method if desired.
-       *
-       * @return A shared pointer to the working instance of Wintermute::Application.
-       */
-      static inline Application* instance() { return self; }
+    virtual ~Application();
+    /**
+     * @fn instance
+     * @brief Provides a pointer to an instance of Wintermute::Application.
+     *
+     * In order to keep Wintermute's instance running smoothly,
+     * a shared pointer to the instance of the application that's running
+     * and handling Wintermute's lower-level work. It's strongly recommended
+     * that you use this method if desired.
+     *
+     * @return A shared pointer to the working instance of Wintermute::Application.
+     */
+    static inline Application* instance() { return self; }
 
-      /**
-       * @fn run
-       * @brief Starts Wintermute.
-       *
-       * This once-invokable method begins the low-level initialization of 
-       * Wintermute. It's invoked from Wintermute's `main()` method and cannot 
-       * be run a second time.
-       */
-      static int run( int& argc, char **argv );
+    /**
+     * @fn run
+     * @brief Starts Wintermute.
+     *
+     * This once-invokable method begins the low-level initialization of
+     * Wintermute. It's invoked from Wintermute's `main()` method and cannot
+     * be run a second time.
+     */
+    static int run( int& argc, char **argv );
 
-      /**
-       * @fn setting
-       * @brief Obtains a setting from Wintermute's local configuration.
-       *
-       * @note These options are global to the Wintermute application.
-       */
-      static QVariant setting(const QString& path, const QVariant defaultValue = QVariant());
+    /**
+     * @fn setting
+     * @brief Obtains a setting from Wintermute's local configuration.
+     *
+     * @note These options are global to the Wintermute application.
+     */
+    static QVariant setting(const QString& path, const QVariant defaultValue = QVariant());
 
-      /** @fn setSetting
-       * @brief Changes a value of the setting for Wintermute
-       *
-       * @note These options are global to the Wintermute application.
-       */
-      static void setSetting(const QString& path, const QVariant value);
+    /** @fn setSetting
+     * @brief Changes a value of the setting for Wintermute
+     *
+     * @note These options are global to the Wintermute application.
+     */
+    static void setSetting(const QString& path, const QVariant value);
 
-      /**
-       * @fn version
-       * @brief Obtains the current version of Wintermute.
-       */
-      Version version() const;
+    /**
+     * @fn version
+     * @brief Obtains the current version of Wintermute.
+     */
+    Version version() const;
 
-      /**
-       * @fn start
-       * @brief
-       *
-       * Starts the initial processing required for execution in Wintermute
-       * and begins the event loop.
-       */
-      Q_SLOT void start();
+    /**
+     * @fn processName
+     * @brief Obtains the stringifed process name used by Wintermute.
+     *
+     * Obtains the process name of this running instance of Wintermute. This
+     * is typically in the form of `domain.application:$PID`, a bit similar
+     * to package paths in Java. The :$PID portion is added to help increase
+     * the precision of finding specific processes. Process names are formed
+     * from the formulated name of the 'root module'.
+     *
+     * @see setRootModule()
+     * @see rootModule()
+     */
+    QString processName() const;
 
-      /**
-       * @fn stop
-       * @brief
-       *
-       * Stops the started processes in `start()` and begins the clean up for
-       * the event loop.
-       */
-      Q_SLOT void stop();
+    /**
+     * @fn module()
+     * @brief Defines the root module of the process.
+     */
+    Procedure::Module* module() const;
+
+    /**
+     * @fn setModule()
+     * @brief Sets the module to be used by this process.
+     *
+     * @note This works only the *first* time it's called. On subsequent
+     * invocations, it just logs out to the console that the method was
+     * used again.
+     */
+    void setModule(Procedure::Module* module);
+
+    /**
+     * @fn start
+     * @brief
+     *
+     * Starts the initial processing required for execution in Wintermute
+     * and begins the event loop.
+     */
+    Q_SLOT void start();
+
+    /**
+     * @fn stop
+     * @brief
+     *
+     * Stops the started processes in `start()` and begins the clean up for
+     * the event loop.
+     */
+    Q_SLOT void stop();
   };
 }
 
