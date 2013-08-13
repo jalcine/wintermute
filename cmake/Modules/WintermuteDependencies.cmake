4f164efd00925560b56b13c025b509c2db94329c
Jacky Alciné <me@jalcine.me>
2013-08-13 01:36:26 -0400

    Updated logic a bit.

--- cmake/Modules/WintermuteDependencies.cmake
+++ cmake/Modules/WintermuteDependencies.cmake
@@ -23,17 +23,19 @@ else()
   set(_wntr_deps TRUE)
 endif()
 
+# Include necessary modules.
 include(FeatureSummary)
+include(FindPkgConfig)
 
 ## Pass options to manipulate Wintermute's dependency.
-option(PROVIDE_GUI_SUPPORT      "Compile Wintermute with QtGui libraries linked and used as run-time." ON)
+option(PROVIDE_GUI_SUPPORT "Compile Wintermute with QtGui libraries linked and used as run-time." ON)
 
 ## Define variables necessary for Qt discovery.
-set(WINTERMUTE_QT_VERSION "4.7")
+set(WINTERMUTE_QT_VERSION 4.7)
 
 ## {{{ Look for Qt, if not found already.
 
-if (NOT DEFINED QT_FOUND OR NOT "${WINTERMUTE_QT_VERSION}" EQUAL "${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH}")
+if (NOT DEFINED QT_FOUND OR NOT ${WINTERMUTE_QT_VERSION} EQUAL "${QT_VERSION_MAJOR}.${QT_VERSION_MINOR}.${QT_VERSION_PATCH}")
   find_package(Qt4 ${WINTERMUTE_QT_VERSION} COMPONENTS
     QtCore
     REQUIRED)
@@ -46,6 +48,8 @@ endif()
 find_package(Log4Qt REQUIRED)
 find_package(QCommandLine REQUIRED)
 
+pkg_check_modules(ZMQ REQUIRED libzmq)
+
 ## }}}
 
 ## {{{ Determine which features and dependencies are flipped on/off.
@@ -54,6 +58,7 @@ add_feature_info("GUI" QT_QTGUI_FOUND "Allows for the rendering of graphical ele
 add_feature_info("Command Line" QCOMMANDLINE_FOUND
   "Allows Wintermute to parse the command line.")
 add_feature_info("Logging" Log4Qt_FOUND "Incorporates logging support.")
+add_feature_info("Procedure Calling" ZMQ_FOUND "Incorporates a transparent means of communicating inbetween processes.")
 
 ## }}}
 
