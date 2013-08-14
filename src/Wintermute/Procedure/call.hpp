efc5e93619521a75874af8052ccc1f54e792536f
Jacky Alciné <me@jalcine.me>
2013-08-13 21:18:59 -0400

    Added docs & methods for Call.

--- src/Wintermute/Procedure/call.hpp
+++ src/Wintermute/Procedure/call.hpp
@@ -17,7 +17,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with Wintermute.  If not, see <http://www.gnu.org/licenses/>.
-**/
+ **/
 
 #ifndef WINTERMUTE_CORE_PROCEDURE_CALL_HPP
 #define WINTERMUTE_CORE_PROCEDURE_CALL_HPP
@@ -25,18 +25,78 @@
 #include <functional>
 #include <QtCore/QObject>
 #include <QtCore/QVariant>
+#include <QtCore/QSharedPointer>
 
 namespace Wintermute {
   namespace Procedure {
+    class CallPrivate;
     class Call : public QObject {
       Q_OBJECT;
       Q_DISABLE_COPY(Call);
 
-      public:
-        explicit Call (QObject* parent);
-        virtual ~Call ();
+      protected:
+      Q_DECLARE_PRIVATE(Call);
+      QSharedPointer<CallPrivate> d_ptr;
 
-        typedef std::function<QVariant (QVariantList)> Signature;
+      public:
+      /**
+       * @enum Type
+       *
+       * Defines the potential types a Call can take in the procedural system.
+       */
+      enum Type {
+        Method    = 0x001, // This call is for handling and manipulating methods.
+        Signal    = 0x002, // This call is for interacting with signals on the platform.
+        Existance = 0x003 // This call handles the existence of other things.
+      };
+
+      explicit Call (QObject* parent);
+      virtual ~Call ();
+
+      /**
+       * @fn dispatch
+       * @brief Invokes this call out to the network to its intended receiver.
+       *
+       * Converts this message to a simpler string for RPC digestion and sends
+       * it over the network to be recieved by others.
+       */
+      virtual QVariant dispatch(QVariantList& arguments) final;
+
+      /**
+       * @fn type
+       * @brief Obtains the type of call this is.
+       *
+       * Obtains the type of message that this exposes.
+       */
+      Type type() const;
+
+      /**
+       * @fn toString
+       * @brief Generates a string representation of this call.
+       */
+      QString toString() const;
+
+      /**
+       * @fn recipient
+       * @brief Obtains the name of the module to recieve this message.
+       *
+       * Obtains the qualified string name that this Call is sending a message
+       * to.
+       */
+      QString recipient() const;
+
+      /**
+       * @fn setRecipient
+       * @brief Changes the recipient of this Call.
+       */
+      void setRecipient(const QString& moduleName);
+
+      /**
+       * @typedef Signature
+       *
+       * Provides the short-hand signature for Call methods.
+       */
+      typedef std::function<QVariant (QVariantList)> Signature;
     };
   } /* Procedure */
 } /* Wintermute */
