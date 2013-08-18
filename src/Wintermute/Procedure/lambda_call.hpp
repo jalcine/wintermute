4da6c9b5098446df08d2efc74f1cea01b235e344
Jacky Alciné <me@jalcine.me>
2013-08-18 10:30:18 -0400

    Added a Call object that can handle lambdas.

---
+++ src/Wintermute/Procedure/lambda_call.hpp
@@ -0,0 +1,38 @@
+/**
+ * vim: ft=qt.cpp
+ * Copyright (C) 2013 Jacky Alcine <me@jalcine.me>
+ *
+ * This file is part of Wintermute, the extensible AI platform.
+ *
+ *
+ * Wintermute is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wintermute is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Wintermute.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+#ifndef WINTERMUTE_CORE_PROCEDURE_LAMBDA_CALL_HPP
+#define WINTERMUTE_CORE_PROCEDURE_LAMBDA_CALL_HPP
+
+#include <Wintermute/Procedure/Call>
+
+namespace Wintermute {
+  namespace Procedure {
+    class LambdaCall : public Call {
+      public:
+        explicit LambdaCall(Call::Signature lambda, QString name);
+        virtual ~LambdaCall();
+        virtual QString name() const;
+    };
+  }
+}
+
+#endif
