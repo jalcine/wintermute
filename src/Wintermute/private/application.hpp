3ae1ab2337c7044cc42a6409dff962198fcb1a65
Jacky Alciné <me@jalcine.me>
2013-08-23 17:03:49 -0400

    Updated Application private data.
      Added logic to handle the process module and beginning logic for determining
      what kind of process this is from command-line arguments.

--- src/Wintermute/private/application.hpp
+++ src/Wintermute/private/application.hpp
@@ -19,45 +19,75 @@
  * along with Wintermute.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
-#include <QtCore/QSharedPointer>
-#include <QtCore/QCoreApplication>
-#include <QtCore/QSettings>
-#include <zmq.hpp>
 #include "Wintermute/logging.hpp"
 #include "Wintermute/arguments.hpp"
 #include "Wintermute/factory.hpp"
 #include "Wintermute/private/Procedure/module.hpp"
+#include "Wintermute/Procedure/process_module.hpp"
+#include <QtCore/QSharedPointer>
+#include <QtCore/QCoreApplication>
+#include <QtCore/QSettings>
+#include <QtZeroMQ/globals.hpp>
+
+#define WINTERMUTE_PLUGIN_MODULE_HEARTBEAT "me.jalcine.wintermute.heartbeat"
 
 namespace Wintermute {
   class ApplicationPrivate {
     friend class Wintermute::Procedure::ModulePrivate;
 
     public:
-      QSharedPointer<QCoreApplication> app;
-      QSharedPointer<Procedure::Module> module;
-      QList<Procedure::Module*> modules;
-      QSettings* settings;
+    QSharedPointer<QCoreApplication> app;
+    QSharedPointer<Procedure::ProcessModule> module;
+    QList<Procedure::Module*> modules;
+    QSettings* settings;
 
-      ApplicationPrivate(int &argc, char **argv) : settings(0) {
-        app = QSharedPointer<QCoreApplication>(new QCoreApplication(argc,argv));
-        module.clear();
-        Procedure::ModulePrivate::context = new zmq::context_t(1);
+    ApplicationPrivate(int &argc, char **argv) : settings(0) {
+      app = QSharedPointer<QCoreApplication>(new QCoreApplication(argc,argv));
+      module.clear();
+    }
+
+    void initialize(){
+      // Add library paths for plug-ins.
+      app->addLibraryPath(WINTERMUTE_PLUGIN_LIBRARY_DIR);
+
+      // Define context for platform.
+      Procedure::ModulePrivate::context = QtZeroMQ::createDefaultContext(Wintermute::Application::instance());
+
+      // Allocate necessary variables for logging and arguments.
+      // TODO: Move factory initialization to separate thread.
+      Logging::instance();
+      Arguments::instance();
+      Factory::instance();
+    }
+
+    int exec(){
+      return app->exec();
+    }
+
+    void loadProcessModule() {
+      // Create the module.
+      Procedure::ProcessModule* modulePtr = new Procedure::ProcessModule;
+      module = QSharedPointer<Procedure::ProcessModule>(modulePtr);
+
+      // TODO: Say 'hello!'.
+      module->invoke("hello", QVariantList());
+    }
+
+    void loadCurrentMode() {
+      // Load each instance.
+      const QString mode = Arguments::instance()->argument("mode").toString();
+
+      if (mode == "daemon" || mode == "d"){
+        wdebug(Application::instance(), "Starting daemon mode...");
+        Factory::instance()->start();
+
+        if (!Arguments::instance()->hasArgument("fork")){
+          // TODO: Fork this process.
+          // TODO: Exit this process.
+        }
+      } else {
+        Factory::instance()->loadPlugin(WINTERMUTE_PLUGIN_MODULE_HEARTBEAT);
       }
-
-      void initialize(){
-        // Add library paths for plug-ins.
-        app->addLibraryPath(WINTERMUTE_PLUGIN_LIBRARY_DIR);
-
-        // Allocate necessary variables for logging and arguments.
-        // TODO: Move factory initialization to separate thread.
-        Logging::instance();
-        Arguments::instance();
-        Factory::instance();
-      }
-
-      int exec(){
-        return app->exec();
-      }
+    }
   };
-
 }
