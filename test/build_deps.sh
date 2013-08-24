e5a295eda47e017646f863a93716cf455ca657a6
Jacky Alciné <me@jalcine.me>
2013-08-24 05:24:22 -0400

    Updated.

--- test/build_deps.sh
+++ test/build_deps.sh
@@ -2,15 +2,16 @@
 
 alias cm="mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr; cd .."
 alias db="make -C build all && sudo make -C build install; cd .."
+
 # Build qcommandline
 # TODO: Add this from a PPA instead
-git clone git://github.com/jalcine/qcommandline && cd qcommandline
-cm && db
+git clone git://github.com/jalcine/qcommandline
+cd qcommandline && cm && db
 
 # Build log4qt
 # TODO: Add this from a PPA instead.
-git clone git://gitorious.org/log4qt/log4qt.git && cd log4qt
-cm && db
+git clone git://gitorious.org/log4qt/log4qt.git
+cd log4qt && cm && db
 
 git clone git://github.com/zeromq/cppzmq
 sudo install cppzmq/zmq.hpp /usr/include/zmq.hpp
@@ -18,6 +19,6 @@ sudo install cppzmq/zmq.hpp /usr/include/zmq.hpp
 # Build QtZeroMQ.
 # TODO: Add this from a PPA.
 git clone git://github.com/jalcine/QtZeroMQ
-cm && db
+cd QtZeroMQ && cm && db
 
 echo "Done loading dependencies."
