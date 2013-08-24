8442c03b8d22c85d100e8c864f465cbcdb759fbb
Jacky Alciné <me@jalcine.me>
2013-08-24 05:10:35 -0400

    Refactored build script.

--- test/build_deps.sh
+++ test/build_deps.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/env sh
 
+alias cm="mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr && cd .."
+alias db="make -C build all && sudo make -C build install && cd .."
 # Build qcommandline
 # TODO: Add this from a PPA instead
-git clone git://github.com/jalcine/qcommandline
-cd qcommandline
-mkdir build
-cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug
-make
-sudo make install
-cd ..
+git clone git://github.com/jalcine/qcommandline && cd qcommandline
+cm && db
 
 # Build log4qt
 # TODO: Add this from a PPA instead.
-git clone git://gitorious.org/log4qt/log4qt.git
-cd log4qt
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=/usr -DCMAKE_BUILD_TYPE=Debug
-make
-sudo make install
-cd ..
+git clone git://gitorious.org/log4qt/log4qt.git && cd log4qt
+cm && db
 
 git clone git://github.com/zeromq/cppzmq
 sudo install cppzmq/zmq.hpp /usr/include/zmq.hpp
@@ -28,13 +18,7 @@ sudo install cppzmq/zmq.hpp /usr/include/zmq.hpp
 # Build QtZeroMQ.
 # TODO: Add this from a PPA.
 git clone git://github.com/jalcine/QtZeroMQ
-cd QtZeroMQ
-mkdir build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr
-make
-sudo make install
-cd ..
+cm && db
 
 # Install coveralls's command line tool.
 sudo pip install cpp-coveralls --use-mirrors
