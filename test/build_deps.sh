e96bee7793e2caa4575cc579eb23d29d2caa0667
Jacky Alciné <me@jalcine.me>
2013-08-24 05:17:50 -0400

    ignore if it's built.

--- test/build_deps.sh
+++ test/build_deps.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-alias cm="mkdir build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr && cd .."
+alias cm="mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr && cd .."
 alias db="make -C build all && sudo make -C build install && cd .."
 # Build qcommandline
 # TODO: Add this from a PPA instead
