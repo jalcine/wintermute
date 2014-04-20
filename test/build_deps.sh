319bbd9276052d3dd6d2ea10baf66f512d9e3ee0
Jacky Alciné <me@jalcine.me>
2014-04-19 23:00:35 -0400

    Touched travis.ci config.

--- test/build_deps.sh
+++ test/build_deps.sh
@@ -3,22 +3,15 @@
 alias cm="mkdir -p build && cd build && cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/usr; cd .."
 alias db="make -C build all && sudo make -C build install; cd .."
 
-# Build qcommandline
-# TODO: Add this from a PPA instead
 git clone git://gitorious.org/qcommandline/qcommandline.git ~/qcommandline
-cd ~/qcommandline && cm && db
-
-# Build log4qt
-# TODO: Add this from a PPA instead.
 git clone git://gitorious.org/log4qt/log4qt.git ~/log4qt
+
+cd ~/qcommandline && cm && db
 cd ~/log4qt && cm && db
 
-git clone git://github.com/zeromq/cppzmq ~/cppzmq
-sudo install ~/cppzmq/zmq.hpp /usr/include/zmq.hpp
-
-# Build QtZeroMQ.
-# TODO: Add this from a PPA.
-git clone git://github.com/jalcine/QtZeroMQ ~/QtZeroMQ --branch develop
-cd ~/QtZeroMQ && cm && db
+#git clone git://github.com/zeromq/cppzmq ~/cppzmq
+#sudo install ~/cppzmq/zmq.hpp /usr/include/zmq.hpp
+#git clone git://github.com/jalcine/QtZeroMQ ~/QtZeroMQ --branch develop
+#cd ~/QtZeroMQ && cm && db
 
 echo "Done loading dependencies."
