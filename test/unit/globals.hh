8e3d114c1c152d4680955d4c25ef84accf318530
Jacky Alciné <me@jalcine.me>
2014-07-19 14:39:33 -0400

    Add singleton macro and test for it.

---
+++ test/unit/globals.hh
@@ -0,0 +1,18 @@
+#include "globals.hpp"
+#include <cxxtest/TestSuite.h>
+
+class SingletonClass {
+  W_DEFINE_SINGLETON(SingletonClass);
+};
+
+W_DECLARE_SINGLETON(SingletonClass);
+
+
+class SingletonTestSuite : public CxxTest::TestSuite
+{
+public:
+  void testIsASingleton(void)
+  {
+    TS_ASSERT ( SingletonClass::instance() == SingletonClass::instance() );
+  }
+};
